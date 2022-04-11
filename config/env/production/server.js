99c9c21a36403e2737520d4e3d8d4c060c3d45d0
Jarosław Filipiak <info@j-filipiak.pl>
2022-04-11 11:24:28 +0200

    update package.json

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
     url: env('MY_HEROKU_URL'),
     proxy: true,
   app: {
-    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+    keys: env.array("APP_KEYS", ['8Rks9H4ByiY6VWkhPkMLmw==' ,'dx9zD7r++U5/C68Q0M60Cw==' ,'CnoIDz9Hn4k8N8TUs2u6gA==' ,'fnJpH/cFhTHl552vSdp29Q==']),
   },
   });
